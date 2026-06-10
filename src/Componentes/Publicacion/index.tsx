5ef79290a3b6a51dd468c47e0095d0b2afa2cdd1
FELIPE MANUEL ANDRACA <48803376@ort>
2026-06-10 09:45:19 -0300

    funcionalidad de likes agregada

--- src/Componentes/Publicacion/index.tsx
+++ src/Componentes/Publicacion/index.tsx
@@ -2,16 +2,29 @@ import type { typePublicacion } from "../../types"
 import Comentario from "../comentario"
 import FotoPerfil from "../FotoPerfil"
 import './publicacion.css'
+import like from '../../fotos/like.png'
+import mensajes from '../../fotos/mensaje.png'
+import enviar from '../../fotos/Enviar.png'
+import { useState } from "react"
+import BotonLike from "../BotonLike"
 
 const Publicacion: React.FC<typePublicacion> = ({perfil,foto, likes, comentarios}) =>{
+    const [likesState, setLikeState] = useState(likes)
     return(
        <div className="publicacion">
             <div className="publicacion-head">
-                <FotoPerfil foto={perfil.foto} nombre={perfil.nombre}/>
+                <div className="profile">
+                <FotoPerfil foto={perfil.foto} nombre={perfil.nombre} />
+                </div>
                 <h4>{perfil.nombre}</h4>
             </div>
             <img src={foto} className="publicacion-img" />
-            <h4 className="publicacion-likes">{likes} likes</h4>
+            <div className="botones">
+                <BotonLike setLikeState={setLikeState} likes={likesState}/>
+                <img src={mensajes}/>
+                <img src={enviar}/>
+            </div>
+            <h4 className="publicacion-likes">{likesState} likes</h4>
             <div className="publicacion-comentario">
               <Comentario nombre={comentarios[0].nombre} comentario={comentarios[0].comentario}/>
             </div>
